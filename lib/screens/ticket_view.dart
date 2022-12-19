0717169e50297826b21def6814baf32edfda827c
Abdul Awal <abdulawalarif0011@gmail.com>
2022-12-19 09:59:42 +0600

    apps screen size is handled in using get pack

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -17,9 +17,9 @@ class TicketView extends StatelessWidget {
     final size = AppLayout.getSize(context);
     return SizedBox(
       width: size.width * .85,
-      height: 200,
+      height: AppLayout.getHeight(200),
       child: Container(
-        margin: const EdgeInsets.only(right: 16),
+        margin: EdgeInsets.only(right: AppLayout.getHeight(16)),
         child: Column(
           children: [
             /*
@@ -28,10 +28,10 @@ class TicketView extends StatelessWidget {
             Container(
               decoration: BoxDecoration(
                   color: const Color(0xFF526799),
-                  borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(21),
-                      topRight: Radius.circular(21))),
-              padding: const EdgeInsets.all(16),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(AppLayout.getHeight(21)),
+                      topRight: Radius.circular(AppLayout.getHeight(21)))),
+              padding: EdgeInsets.all(AppLayout.getHeight(16)),
               child: Column(
                 children: [
                   Row(
@@ -47,7 +47,7 @@ class TicketView extends StatelessWidget {
                           child: Stack(
                         children: [
                           SizedBox(
-                            height: 24,
+                            height: AppLayout.getHeight(24),
                             child: LayoutBuilder(
                               builder: (BuildContext context,
                                   BoxConstraints constraints) {
@@ -96,7 +96,7 @@ class TicketView extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       SizedBox(
-                        width: 100,
+                        width: AppLayout.getWidth(100),
                         child: Text(
                           ticket['from']['name'],
                           style: Styles.headLineStyle4.copyWith(
@@ -110,7 +110,7 @@ class TicketView extends StatelessWidget {
                             Styles.headLineStyle4.copyWith(color: Colors.white),
                       ),
                       SizedBox(
-                        width: 100,
+                        width: AppLayout.getWidth(100),
                         child: Text(
                           ticket['to']['name'],
                           textAlign: TextAlign.end,
@@ -132,19 +132,21 @@ class TicketView extends StatelessWidget {
               child: Row(
                 children: [
                   SizedBox(
-                    height: 20,
-                    width: 10,
+                    height: AppLayout.getHeight(20),
+                    width: AppLayout.getWidth(10),
                     child: DecoratedBox(
                         decoration: BoxDecoration(
                             color: Colors.white,
                             borderRadius: BorderRadius.only(
-                              topRight: Radius.circular(10),
-                              bottomRight: Radius.circular(10),
+                              topRight:
+                                  Radius.circular(AppLayout.getHeight(10)),
+                              bottomRight:
+                                  Radius.circular(AppLayout.getHeight(10)),
                             ))),
                   ),
                   Expanded(
                     child: Padding(
-                      padding: const EdgeInsets.all(12.0),
+                      padding: EdgeInsets.all(AppLayout.getHeight(12)),
                       child: LayoutBuilder(
                         builder:
                             (BuildContext context, BoxConstraints constraints) {
@@ -169,14 +171,15 @@ class TicketView extends StatelessWidget {
                     ),
                   ),
                   SizedBox(
-                    height: 20,
-                    width: 10,
+                    height: AppLayout.getHeight(20),
+                    width: AppLayout.getWidth(10),
                     child: DecoratedBox(
                         decoration: BoxDecoration(
                             color: Colors.white,
                             borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(10),
-                              bottomLeft: Radius.circular(10),
+                              topLeft: Radius.circular(AppLayout.getHeight(10)),
+                              bottomLeft:
+                                  Radius.circular(AppLayout.getHeight(10)),
                             ))),
                   )
                 ],
@@ -190,11 +193,14 @@ class TicketView extends StatelessWidget {
             Container(
               decoration: BoxDecoration(
                   color: Styles.orangeColor,
-                  borderRadius: const BorderRadius.only(
-                      bottomLeft: Radius.circular(21),
-                      bottomRight: Radius.circular(21))),
-              padding: const EdgeInsets.only(
-                  left: 16, top: 10, right: 16, bottom: 16),
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(AppLayout.getHeight(21)),
+                      bottomRight: Radius.circular(AppLayout.getHeight(21)))),
+              padding: EdgeInsets.only(
+                  left: AppLayout.getHeight(16),
+                  top: AppLayout.getHeight(10),
+                  right: AppLayout.getHeight(16),
+                  bottom: AppLayout.getHeight(16)),
               child: Column(
                 children: [
                   Row(
--- lib/utils/app_layout.dart
+++ lib/utils/app_layout.dart
@@ -1,7 +1,30 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
 
 class AppLayout {
   static getSize(BuildContext context) {
     return MediaQuery.of(context).size;
+  }
+
+  static getScreenHeight() {
+    return Get.height;
+  }
+
+  static getScreenWidth() {
+    return Get.width;
+  }
+
+  //for specific portion of screen divide and getting it device wise
+  // Handling Screen size for different device
+  static getHeight(double pixels) {
+    double x = getScreenHeight() / pixels; //844/200=>4.22
+    return getScreenHeight() / x;
+  }
+
+  static getWidth(double pixels) {
+    double x = getScreenWidth() / pixels; //844/200=>4.22
+    return getScreenWidth() / x;
   }
 }
